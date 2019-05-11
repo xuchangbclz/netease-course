b2d21311c37699eab406285a0e0590d428b06e7a
daituo <daituo123231757@163.com>
2019-05-11 12:12:15 +0800

    提交

---
+++ src/main/java/com/netease/course/neteasecourse/高性能编程专题/多线程并发编程/java基础/线程安全原子操作/AtomicDemo.java
@@ -0,0 +1,14 @@
+package com.netease.course.neteasecourse.高性能编程专题.多线程并发编程.java基础.线程安全原子操作;
+
+/**
+ *  volatile 只能保证变量的可见性，不能保证变量的原子操作性
+ *
+ *  原子性本质是数据保持一致性
+ *
+ *
+ *
+ *
+ **/
+public class AtomicDemo {
+
+}
